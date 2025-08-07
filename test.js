80aa80a756d4934aea41bc7fc5b747c5db155e25
Neelam Kolhe <kolheneelam15@gmail.com>
2025-08-07 15:39:16 +0530

    Changes

--- test.js
+++ test.js
@@ -4,14 +4,12 @@ function log(message) {
   console.log(`${new Date().toLocaleTimeString()}: ${message}`);
 }
 
-// Trailing example
 console.log("=== Trailing Test ===");
 const trailing = debounce(() => log("Trailing executed"), 1000);
 trailing();
 setTimeout(trailing, 300);
 setTimeout(trailing, 700);
 
-// Leading example
 setTimeout(() => {
   console.log("\n=== Leading Test ===");
   const leading = debounce(() => log("Leading executed"), 1000, true);
